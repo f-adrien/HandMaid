965bef8b22a1f2c628accc19d088070d2c4ba968
Abs Yade <absyade@gmail.com>
2019-08-20 11:33:18 +0200

    FEAT: generated model Cleaner + migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_091538) do
+ActiveRecord::Schema.define(version: 2019_08_20_093212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cleaners", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.boolean "availability"
+    t.string "location"
+    t.integer "radius"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "clients", force: :cascade do |t|
     t.string "first_name"
