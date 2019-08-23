f4d91b41804dfc23aef240518e2ecdbce4dbcf9a
Barbara Peric <b.peric10@gmail.com>
2019-08-23 13:39:09 +0200

    FIX: reqest pending page

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,18 +3,10 @@ class RequestsController < ApplicationController
   def show
   end
 
- #  def update
- #    @request = Request.find(params[:id])
- #    @request.update(request_params)
- #    redirect_to edit_request_path(@request)
- #  end
-
- #  private
-
- #  def request_params
- #    params.require(:request).permit(:first_name, :last_name, :address, :radius, :phone_number)
- # end
+  def update
   end
+
+end
 
 
 
