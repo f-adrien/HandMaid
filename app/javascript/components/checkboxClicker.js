6603024a9982623596f13e159a58a9719ff29710
Abs Yade <absyade@gmail.com>
2019-08-26 14:37:01 +0200

    FEAT: Frontend and JavaScript on booking + seed

--- app/javascript/components/checkboxClicker.js
+++ app/javascript/components/checkboxClicker.js
@@ -5,17 +5,22 @@
  // When clicked, click on the hidden checkbox but with javascript
  // toggle active class
 
-
+const cards = document.querySelectorAll(".card-service");
 const clicker = () => {
-  const services = document.querySelectorAll(".service");
-  services.forEach(service => {
-    service.addEventListener("click", (event) => {
-      const input = document.getElementById(event.currentTarget.htmlFor)
-      input.checked = !input.checked
-      input.click();
-      service.classList.toggle("services-icons-checked")
+  if (cards) {
+    cards.forEach(card => {
+      const service = card.querySelector('.service')
+      const input = card.getElementsByTagName('input')[0]
+      const serviceName = card.getElementsByTagName('p')[0]
+      service.addEventListener("click", (event) => {
+        input.checked = !input.checked
+        input.click();
+        // service.classList.toggle("services-icons-checked")
+        card.classList.toggle("services-icons-checked")
+        serviceName.classList.toggle("service-name")
+      })
     })
-  })
+  }
 }
 
 export { clicker }
