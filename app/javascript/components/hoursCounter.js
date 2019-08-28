2030d5469c77a30670c3d0858077f6e8f2216f9b
Barbara Peric <b.peric10@gmail.com>
2019-08-28 10:31:08 +0200

    FEAT: javascript for counter

---
+++ app/javascript/components/hoursCounter.js
@@ -0,0 +1,25 @@
+const addHrsBtn = document.querySelector("#js-plus-hours");
+const subHrsBtn = document.querySelector("#js-minus-hours");
+const formInput = document.querySelector("#booking_total_duration");
+// const currentValie = document ....
+
+const addHours = (event) => {
+  event.preventDefault()
+  console.log("hello");
+  formInput.value = parseInt(formInput.value) + 1
+}
+
+const subHours = (event) => {
+  event.preventDefault()
+  formInput.value = parseInt(formInput.value) - 1
+}
+
+const counter = () => {
+  addHrsBtn.addEventListener("click", addHours);
+  subHrsBtn.addEventListener("click", subHours);
+}
+
+export { counter }
+
+
+
