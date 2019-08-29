f0c55e4e71bc792a2a8d5eef4475f633489f9539
Barbara Peric <b.peric10@gmail.com>
2019-08-29 10:05:31 +0200

    FIX: refactoring js files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,6 @@
 import "bootstrap";
 import 'plugins/flatpicker';
 import { initAutocomplete } from '../plugins/init_autocomplete';
-import { initLocAutocomplete } from '../plugins/init_loc_autocomplete';
-import { initLocClientAutocomplete } from '../plugins/init_loc_client_autocomplete';
 import { clicker } from "../components/checkboxClicker";
 import { counter } from "../components/hoursCounter";
 
@@ -10,6 +8,4 @@ import { counter } from "../components/hoursCounter";
 clicker();
 counter();
 initAutocomplete();
-initLocAutocomplete();
-initLocClientAutocomplete();
 
--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,10 +1,13 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
-  const addressInput = document.getElementById('booking_address');
-  if (addressInput) {
-    places({ container: addressInput });
-  }
+  ['booking_address', 'cleaner_address', 'client_address'].forEach((id) => {
+    const addressInput = document.getElementById(id);
+    if (addressInput) {
+      places({ container: addressInput });
+    }
+
+  })
 };
 
 
