b065755fa1318c01fca330cb290bd61a628d0ced
Francois Adrien <francois.s.adrien@gmail.com>
2019-08-29 11:19:41 +0200

    Ajaxify pending bookings

--- app/javascript/components/hoursCounter.js
+++ app/javascript/components/hoursCounter.js
@@ -3,21 +3,25 @@ const subHrsBtn = document.querySelector("#js-minus-hours");
 const formInput = document.querySelector("#booking_total_duration");
 // const currentValie = document ....
 
-const addHours = (event) => {
-  event.preventDefault()
-  console.log("hello");
-  formInput.value = parseInt(formInput.value) + 1
-}
+  const addHours = (event) => {
+    event.preventDefault()
+    console.log("hello");
+    formInput.value = parseInt(formInput.value) + 1
+  }
 
-const subHours = (event) => {
-  event.preventDefault()
-  formInput.value = parseInt(formInput.value) - 1
-}
+  const subHours = (event) => {
+    event.preventDefault()
+    formInput.value = parseInt(formInput.value) - 1
+  }
 
 const counter = () => {
-  addHrsBtn.addEventListener("click", addHours);
-  subHrsBtn.addEventListener("click", subHours);
+  if (formInput) {
+    addHrsBtn.addEventListener("click", addHours);
+    subHrsBtn.addEventListener("click", subHours);
+  }
 }
+
+
 
 export { counter }
 
