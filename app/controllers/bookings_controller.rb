a7df8a730f2a69993c0a7c4f680bac514dc1a9a8
Francois Adrien <francois.s.adrien@gmail.com>
2019-08-30 09:35:47 +0200

    last vixes 2

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,8 +3,8 @@ class BookingsController < ApplicationController
   before_action :find_booking, only: [:show, :payment]
 
   def index
-    @bookings_confirmed = current_user.bookings.where(status: "confirmed")
-    @bookings_pending = current_user.bookings.where(status: "pending")
+    @bookings_confirmed = current_user.bookings.where(status: "confirmed").order(:date).reverse
+    @bookings_pending = current_user.bookings.where(status: "pending").order(:date).reverse
   end
 
   def show
